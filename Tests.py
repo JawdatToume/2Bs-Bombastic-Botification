d300e6ac63679ef441c835f1495a039b9dfef062
Alikay <asalm@ualberta.ca>
2021-12-07 04:31:20 -0700

    fixed directory

--- Tests.py
+++ Tests.py
@@ -5,7 +5,7 @@ races = ["zerg", "protoss", "terran"]
 maps = ["CactusValleyLE.SC2Map", "BelShirVestigeLE.SC2Map", "ProximaStationLE.SC2Map"]
 
 def getFileName(race, diff, map, iter):
-    return "build\\" + diff + "-" + race + "-" + map + "-" + str(iter) + ".txt"
+    return "tests\\" + diff + "-" + race + "-" + map + "-" + str(iter) + ".txt"
 
 iter = 0
 while True:
@@ -13,5 +13,5 @@ while True:
     for diff in difficulties:
         for race in races:
             for map in maps:
-                command = ".\build\bin\BasicSc2Bot.exe -c -a " + race + " -d " + diff + " -m " + map + " > " + getFileName(race, diff, map, iter)
+                command = ".\\build\\bin\\BasicSc2Bot.exe -c -a " + race + " -d " + diff + " -m " + map + " > " + getFileName(race, diff, map, iter)
                 os.system(command)
