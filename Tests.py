a26d76071516273b51bde15d01465ceab018bd5b
Alikay <asalm@ualberta.ca>
2021-12-07 04:24:07 -0700

    added tests python program

---
+++ Tests.py
@@ -0,0 +1,17 @@
+import os
+
+difficulties = ["Easy", "Medium", "Hard"]
+races = ["zerg", "protoss", "terran"]
+maps = ["CactusValleyLE.SC2Map", "BelShirVestigeLE.SC2Map", "ProximaStationLE.SC2Map"]
+
+def getFileName(race, diff, map, iter):
+    return "build\\" + diff + "-" + race + "-" + map + "-" + str(iter) + ".txt"
+
+iter = 0
+while iter == 0:
+    iter += 1
+    for diff in difficulties:
+        for race in races:
+            for map in maps:
+                command = ".\build\bin\BasicSc2Bot.exe -c -a " + race + " -d " + diff + " -m " + map + " > " + getFileName(race, diff, map, iter)
+                os.system(command)
